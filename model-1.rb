c5c1f9937227cd56f595817a6e55d95e16434cfa
Hannah Warren <hannahkwarren@gmail.com>
2021-08-30 22:25:56 -0400

    Add integers, floats, arrays

--- model-1.rb
+++ model-1.rb
@@ -1,3 +1,13 @@
 city = "New York"
 state = "New York"
 counties = [Kings, Manhattan, Bronx, Queens, Richmond]
+
+population_millions = 8.419
+unemployment_rate_jun21 = 10.9
+boroughs = 5
+
+landlocked = false
+public_beach_access = true
+
+neighborhoods_bk = ["Clinton Hill", "DUMBO", "Fort Greene", "Crown Heights", "Prospect Heights", "Carroll Gardens", "Downtown Brooklyn"]
+neighborhoods_man = ["NoLiTa", "FiDi", "Chinatown", "Tribeca", "Theater District", "UES", "UWS", "Harlem", "El Barrio", "Washington Heights", "Inwood", "Kips Bay", "Soho"]

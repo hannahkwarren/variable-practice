b896cd46e1a1c8b97fd0d74e7309004ef04a9246
Hannah Warren <hannahkwarren@gmail.com>
2021-08-30 22:40:04 -0400

    Add a few more neighborhoods

--- model-1.rb
+++ model-1.rb
@@ -9,7 +9,7 @@ boroughs = 5
 landlocked = false
 public_beach_access = true
 
-neighborhoods_bk = ["Clinton Hill", "DUMBO", "Fort Greene", "Crown Heights", "Prospect Heights", "Carroll Gardens", "Downtown Brooklyn"]
-neighborhoods_man = ["NoLiTa", "FiDi", "Chinatown", "Tribeca", "Theater District", "UES", "UWS", "Harlem", "El Barrio", "Washington Heights", "Inwood", "Kips Bay", "Soho"]
+neighborhoods_bk = ["Clinton Hill", "DUMBO", "Fort Greene", "Crown Heights", "Prospect Heights", "Carroll Gardens", "Downtown Brooklyn", "Brooklyn Heights", "Cobble Hill", "Boerum Hill"]
+neighborhoods_man = ["NoLiTa", "FiDi", "Chinatown", "Tribeca", "Theater District", "UES", "UWS", "Harlem", "El Barrio", "Washington Heights", "Inwood", "Kips Bay", "Soho", "Lower East Side"]
 
 state_trivia = {flower: 'Rose', fruit: 'Apple', gem: 'Garnet', slogan: 'I Love New York', tree: 'sugar maple', bird: 'bluebird', fossil: 'Sea scorpion'}
